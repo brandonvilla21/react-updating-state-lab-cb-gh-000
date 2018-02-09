4915e7e972a10ed21318f0bb706807c717760174
Brandon Villa <bornofos@gmail.com>
2018-02-09 23:39:45 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,15 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+class ClassName extends React.Component {
+  constructor() {
+    this.state = {
+      errors: [], 
+      user: null, 
+      settings: { 
+        bitrate: 8, 
+        video: { resolution: '1080p' } 
+      }
+    }
+  }
+}
