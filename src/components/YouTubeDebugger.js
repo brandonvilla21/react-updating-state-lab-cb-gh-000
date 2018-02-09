820abf4e93738a8fd2cf91e64ae9c01a5d2b302b
Brandon Villa <bornofos@gmail.com>
2018-02-09 23:42:15 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -4,12 +4,26 @@ import React from 'react'
 class ClassName extends React.Component {
   constructor() {
     this.state = {
-      errors: [], 
-      user: null, 
+      errors: [],
+      user: null,
       settings: { 
-        bitrate: 8, 
-        video: { resolution: '1080p' } 
+        bitrate: 8,
+        video: { resolution: '1080p' }
       }
     }
+  }
+  handleClick = () => {
+    this.setState({
+      settings: Object.assign(this.state.settings, {
+        bitrate: 12
+      })
+    })
+  }
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.handleClick}> Click me</button>
+      </div>
+    )
   }
 }
